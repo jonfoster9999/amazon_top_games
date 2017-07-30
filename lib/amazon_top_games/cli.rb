40df36c94e295558db9c61fc90a7d32e177f2005
Jon foster <redcabinny@gmail.com>
2017-07-30 17:35:22 -0400

    updated cli.rb

--- lib/amazon_top_games/cli.rb
+++ lib/amazon_top_games/cli.rb
@@ -29,13 +29,19 @@ class AmazonTopGames::CLI
 		catch :exit do
 			case input
 			when "1"
-				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Playstation", "playstation_url")).print_list
+
+				#need to call CLI#console_menu(playstation_console)  here
+				#move "AmazonTopGames::ConsoleTopTwentyList.new(playstation_console)" into this method
+				# going to need a print item method which takes the href of the corresponding list_item choice, scrapes it and prints it
+
+				#when printing the individual description we'll take a gets.chomp.to_i   and call the item to be printed by it's index 
+				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Playstation 4", "https://www.amazon.com/Best-Sellers-Video-Games-PlayStation-Consoles-Accessories/zgbs/videogames/6427814011/ref=zg_bs_nav_vg_1_vg/156-9684545-1840139")).print_list
 			when "2"
-				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Xbox", "xbox_url")).print_list
+				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Xbox One", "https://www.amazon.com/Best-Sellers-Video-Games-Xbox-One-Consoles-Accessories/zgbs/videogames/6469269011/ref=zg_bs_nav_vg_1_vg")).print_list
 			when "3"
-				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Wii", "wii_url")).print_list
+				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Wii", "https://www.amazon.com/Best-Sellers-Video-Games-Wii-Consoles-Accessories/zgbs/videogames/14218901/ref=zg_bs_nav_vg_1_vg")).print_list
 			when "4" 
-				AmazonTopGame::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Nintendo DS", "nindendo_DS_url")).print_list
+				AmazonTopGames::ConsoleTopTwentyList.new(AmazonTopGames::Console.new("Nintendo DS", "https://www.amazon.com/Best-Sellers-Video-Games-Nintendo-DS-Consoles-Accessories/zgbs/videogames/11075831/ref=zg_bs_nav_vg_1_vg")).print_list
 			when "exit"
 				throw :exit
 			else
